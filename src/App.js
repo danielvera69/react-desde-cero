c5fd1adce4a298db98d78914824719131f66e46f
Daniel Vera <davparedes69@gmail.com>
2020-05-11 17:29:10 -0500

    Creacion de componente cursos con arrays

--- src/App.js
+++ src/App.js
@@ -9,7 +9,42 @@ import "./styles/styles.scss"
 // 6: for   htmlFor=>
 // 7: No utilizar if, else, while
 import Curso from './Curso'
-const App = () =>(
+
+const cursos = [
+  {
+    "title": "React desde cero",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQXm7lwTlPxpsRvkon6NqjzA5MQxD-DD1w-Fe9KYXfWlFPKu07I&usqp=CAU",
+    "price": 50.00,
+    "profesor": "Daniel Vera",
+    "id": "1"
+ 
+  },
+  {
+    "title": "Go desde cero",
+    "image": "https://edteam-media.s3.amazonaws.com/courses/original/565fa9ec-8f84-448a-b0d8-855d059f16dd.png",
+    "price": 70.00,
+    "profesor": "Raul Berrones",
+    "id": "2"
+ 
+  },
+  {
+    "title": "Angular desde cero",
+    "image": "https://edteam-media.s3.amazonaws.com/courses/original/9e7655f4-a855-4d30-a3e0-736fa47f479b.png",
+    "price": 20.00,
+    "profesor": "Moises Lopez",
+    "id": "3"
+ 
+  },
+  {
+    "title": "Vue desde cero",
+    "image": "https://edteam-media.s3.amazonaws.com/courses/original/b393cce0-3ec8-4c61-877d-92e2e04f9120.png",
+    "price": 30.00,
+    "profesor": "Daniel Vera",
+    "id": "4"
+ 
+  },
+ ]
+const App = () => (
  <>
   <div className="main-banner img-container l-section" id="main-banner">
     <div className="ed-grid lg-grid-6">
@@ -24,13 +59,26 @@ const App = () =>(
     </div>
   </div>
   <div className="ed-grid m-grid-3">
-     <Curso/>
-     <Curso/>
-     <Curso/>
-     <Curso/>
-     <Curso/>
-     <Curso/>
-     <Curso/>
+     {/* <Curso
+        title="React Desde Cero"
+        image='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQXm7lwTlPxpsRvkon6NqjzA5MQxD-DD1w-Fe9KYXfWlFPKu07I&usqp=CAU'
+        price={50}
+        profesor="Daniel Vera"
+        id='1'
+     /> */}
+     {
+       cursos.map(
+         c =>  
+           <Curso
+             title = {c.title}
+             image = {c.image} 
+             price = {c.price} 
+             profesor = {c.profesor} 
+             id = {c.id} 
+           />
+        )
+    }
+     
   </div>
  </> 
 )
--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,28 +1,43 @@
 import React from 'react'
+// define valores por default
+import PropTypes  from 'prop-types'
+// el JSX utiliza el componente react en su estructura
+// const curso = {
+//   'title':'React desde cero',
+//   'image':'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQXm7lwTlPxpsRvkon6NqjzA5MQxD-DD1w-Fe9KYXfWlFPKu07I&usqp=CAU',
+//   'price':'50usd'
+// }
+// Se puede recibir el parametro (props) o destructurando el props
 
-const Curso = () => (
-    <article class="card">
-      <div class="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-        <img src="https://img.icons8.com/clouds/4ac144/256/music.png" alt="foto1"/>
+const Curso = ({title, image, price, profesor,id}) => (
+   <article className="card" id={id}>
+      <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
+         <img src={image} alt={title}/>
       </div>
-      <div class="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-        <h3 class="t5 s-mb-2 s-center">
-          Programación orientada a objetos con Go
-        </h3>
-        <div class="s-mb-2 s-main-center">
-          <div class="card__teacher s-cross-center">
-            <div class="card__avatar s-mr-1">
-              <div class="circle img-container">
-                <img src="https://img.icons8.com/plasticine/4ac144/256/user-male.png" alt="foto2"/>
-              </div>
-            </div>
-            <span class="small">Alexys Lozada</span>
-          </div>
-        </div>
-        <div class="s-main-center">
-          <a class="button--ghost-alert button--tiny" href="#">$ 20USD</a>
-        </div>
+      <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
+         <h3 className="center">{title ? title:'No'}</h3>
+         <div className="s-main-center">
+            <a className="button--ghost-alert button--tiny" href="#">{`$ ${price} USD`}</a>
+         </div>
+         <div className="s-main-center">
+           <h6>{`Profesor: ${profesor}`}</h6>
+         </div>
       </div>
-    </article>
+   </article>
 )
+Curso.propTypes = {
+   title: PropTypes.string,
+   image: PropTypes.string,
+   price: PropTypes.number,
+   profesor: PropTypes.string,
+   id: PropTypes.string
+}
+Curso.defaultProps = {
+   title: "Sin Titulo",
+   image: "Sin imagen",
+   price: 0.00,
+   profesor: "No asignado",
+   id: "0"
+}
+
 export default Curso    
